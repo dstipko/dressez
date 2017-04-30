ed0d51079d7682c655319ca29ee5b5af92786f09
tzadro <tzadro@tzadro.local>
2017-04-30 19:53:02 +0200

    WIP

--- Dressez/NewClothingItemController.swift
+++ Dressez/NewClothingItemController.swift
@@ -25,13 +25,16 @@ class NewClothingItemController: BaseViewController {
     }
     
     var image: UIImage!
-    private var itemType: Int?
-    private var itemColor: Int?
+    fileprivate var itemType: Int?
+    fileprivate var itemColor: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.setup(image: image, imageView: clothingItemImageView)
         presenter.configureTextFields(nameTextField: nameTextField, typeTextField: typeTextField, colorTextField: colorTextField)
+        
+        typeTextField.delegate = self
+        colorTextField.delegate = self
     }
    
     @IBAction func saveClothingItem(_ sender: Any) {
@@ -55,7 +58,8 @@ class NewClothingItemController: BaseViewController {
         }
         
     }
-    @IBAction func showTypeActionSheet(_ sender: Any) {
+    
+    /*@IBAction func showTypeActionSheet(_ sender: Any) {
         let typeMenu = presenter.createAlertController(title: nil, message: StringConstants.selectType, style: .actionSheet)
         for type in ItemType.allValues {
             let colorAction = presenter.createAlertAction(title: type.description(), completionHandler: { _ in
@@ -69,7 +73,6 @@ class NewClothingItemController: BaseViewController {
         self.present(typeMenu, animated: true, completion: nil)
     }
     
-    
     @IBAction func showColorActionSheet(_ sender: Any) {
         let colorsMenu = presenter.createAlertController(title: nil, message: StringConstants.selectColor, style: .actionSheet)
         for color in ItemColor.allValues {
@@ -82,6 +85,39 @@ class NewClothingItemController: BaseViewController {
             colorsMenu.addAction(colorAction)
         }
         self.present(colorsMenu, animated: true, completion: nil)
-        
+    }*/
+}
+
+extension NewClothingItemController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        switch textField {
+        case typeTextField:
+            let typeMenu = presenter.createAlertController(title: nil, message: StringConstants.selectType, style: .actionSheet)
+            for type in ItemType.allValues {
+                let colorAction = presenter.createAlertAction(title: type.description(), completionHandler: { _ in
+                    self.itemType = type.rawValue
+                    self.typeTextField.text = type.description()
+                    self.dismiss(animated: true, completion: nil)
+                    return
+                })
+                typeMenu.addAction(colorAction)
+            }
+            self.present(typeMenu, animated: true, completion: nil)
+        case colorTextField:
+            let colorsMenu = presenter.createAlertController(title: nil, message: StringConstants.selectColor, style: .actionSheet)
+            for color in ItemColor.allValues {
+                let colorAction = presenter.createAlertAction(title: color.description(), completionHandler: { _ in
+                    self.itemColor = color.rawValue
+                    self.colorTextField.text = color.description()
+                    self.dismiss(animated: true, completion: nil)
+                    return
+                })
+                colorsMenu.addAction(colorAction)
+            }
+            self.present(colorsMenu, animated: true, completion: nil)
+        default:
+            return false
+        }
+        return true
     }
 }
