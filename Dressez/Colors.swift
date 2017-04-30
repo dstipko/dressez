0b47c52d751f53846c596cdaaa72c63216cdbf7e
Dora Stipkovic <dora.stip@gmail.com>
2017-04-30 22:15:34 +0200

    separate color constants

--- Dressez/Colors.swift
+++ Dressez/Colors.swift
@@ -1,5 +1,5 @@
 //
-//  StringConstants.swift
+//  Colors.swift
 //  Dressez
 //
 //  Created by Dora Stipković on 4/30/17.
@@ -9,18 +9,10 @@
 import Foundation
 import UIKit
 
-struct StringConstants {
-    
-    static var title = "Add clothing item"
-    static var selectColor = "Select color"
-    static var selectType = "Select type"
-    static var ok = "OK"
-    static var requiredFields = "Empty fields"
-
-}
-
 struct Colors {
+    
     static var barsDefault = UIColor(colorLiteralRed: (247/255), green: (247/255), blue: (247/255), alpha: 1)
     static var darkGray = UIColor(colorLiteralRed: 0.36, green: 0.41, blue: 0.45, alpha: 1.0)
     static var green = UIColor(colorLiteralRed: 0.48, green: 0.66, blue: 0.36, alpha:1.0)
+    
 }
---
+++ Dressez/Strings.swift
@@ -0,0 +1,21 @@
+//
+//  StringConstants.swift
+//  Dressez
+//
+//  Created by Dora Stipković on 4/30/17.
+//  Copyright © 2017 Dora Stipković. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct StringConstants {
+    
+    static var title = "Add clothing item"
+    static var selectColor = "Select color"
+    static var selectType = "Select type"
+    static var ok = "OK"
+    static var requiredFields = "Empty fields"
+
+}
+
