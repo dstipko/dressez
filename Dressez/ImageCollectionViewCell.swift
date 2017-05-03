e80e9336626bb2fccb4b34393e864b149da6f6e6
tzadro <tzadro@tzadro.local>
2017-05-03 17:14:02 +0200

    Aspect fit, minor bug fix & label resize

--- Dressez/ImageCollectionViewCell.swift
+++ Dressez/ImageCollectionViewCell.swift
@@ -14,6 +14,7 @@ class ImageCollectionViewCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        imageView.contentMode = .scaleAspectFit
         layer.cornerRadius = NumberConstants.cornerRadius / 2
         layer.masksToBounds = true
     }
